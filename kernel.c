9bb7dc23ad77a6af53fc688cf5e552b8cf33bfec
Nathan Arnold <natearn@natearn.info>
2013-08-05 12:06:09 -0400

    working fork

--- kernel.c
+++ kernel.c
@@ -13,23 +13,22 @@ void bwputs(char *s) {
 	}
 }
 
-int task1(void) {
+int task(void) {
+	int x;
 	while(1) {
-		bwputs("task1\n");
-		fork();
-		/* yield(); */
+		bwputs("task\n");
+		x = fork();
+		if(x == 0) {
+			bwputs("I am the child\n");
+		} else if(x > 0) {
+			bwputs("I am the parent\n");
+		} else if(x < 0) {
+			bwputs("Something went wrong\n");
+			debug(x,0,0);
+		}
 	}
 	return 0;
 }
-
-int task2(void) {
-	while(1) {
-		bwputs("task2\n");
-		yield();
-	}
-	return 0;
-}
-
 /*
 initialize a process given its stack
 	returns the pointer to the top of the stack (needed by activate)
@@ -41,10 +40,6 @@ unsigned int *init_process(unsigned int *stack, unsigned int size, int (*task)(v
 	return stack + size - 16;
 }
 
-unsigned int reg(unsigned int num, unsigned int stack[]) {
-	return stack[2 + num];
-}
-
 unsigned int scheduler(unsigned int *procs[], unsigned int size, unsigned int active) {
 	unsigned int i;
 	if(procs[size-1] == procs[active]) return 0;
@@ -52,11 +47,11 @@ unsigned int scheduler(unsigned int *procs[], unsigned int size, unsigned int ac
 		if(procs[i] == procs[active]) return i+1;
 	}
 	bwputs("scheduler error\n");
-	while(1);
+	debug(procs, size, active);
 	return 0;
 }
 
-void *memcpy(void *src, size_t size, void* dest) {
+void *memcpy(void* dest, void *src, size_t size) {
 	char *to = dest;
 	const char *from = src;
 	size_t i;
@@ -66,27 +61,33 @@ void *memcpy(void *src, size_t size, void* dest) {
 	return dest;
 }
 
-unsigned int _fork(unsigned int *stacks[], unsigned int *procs[], unsigned int parent, unsigned int next_pid) {
+void debug(unsigned int a, unsigned int b, unsigned int c) {
+	bwputs("debug halt\n");
+	while(1);
+}
+
+unsigned int _fork(unsigned int stacks[][STACK_SIZE], unsigned int *procs[], unsigned int parent, unsigned int next_pid) {
+	unsigned int offset;
 	bwputs("_fork\n");
 	if(next_pid == NUM_STACKS) {
 		procs[parent][2+0] = -1;
 		return next_pid;
 	}
-	/* THIS IS WRONG IN MORE THAN ONE WAY */
-	procs[next_pid] = memcpy(stacks[parent], stacks[parent] - procs[parent], stacks[next_pid]);
+	offset = procs[parent] - stacks[parent];
+	procs[next_pid] = stacks[next_pid] + offset;
+	memcpy(procs[next_pid], procs[parent], sizeof(*procs[parent]) * (STACK_SIZE - offset));
 	procs[parent][2+0] = next_pid;
 	procs[next_pid][2+0] = 0;
-	return next_pid++;
+	return next_pid + 1;
 }
 
 int main(void) {
 	unsigned int user_stack[NUM_STACKS][STACK_SIZE];
-	unsigned int n = 0; /* the next available stack. n should never be >= NUM_STACKS */
 	unsigned int *procs[NUM_STACKS];
+	unsigned int n = 0; /* the next available stack. n should never be >= NUM_STACKS */
 	unsigned int active = 0;
 
-	procs[n++] = init_process(user_stack[n],STACK_SIZE,&task1);
-	procs[n++] = init_process(user_stack[n],STACK_SIZE,&task2);
+	procs[n++] = init_process(user_stack[n],STACK_SIZE,&task);
 	while(1) {
 		/* choose a process to run */
 		active = scheduler(procs, n, active);
