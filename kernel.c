f39eb340619f44cefeb7ffeefe2eae664b74506f
Nathan Arnold <natearn@natearn.info>
2013-08-22 22:10:10 -0400

    created process struct and fixed compiler warnings

--- kernel.c
+++ kernel.c
@@ -4,6 +4,14 @@
 
 #define NUM_STACKS 10
 #define STACK_SIZE 1024
+#define QUEUE_SIZE 256
+
+struct Process {
+	unsigned int stack[STACK_SIZE];
+	unsigned int queue[QUEUE_SIZE];
+	unsigned int *stackptr;
+	unsigned int blocked; /* updgrade this to status when there is more than 2 */
+};
 
 void bwputs(char *s) {
 	while(*s) {
@@ -14,6 +22,7 @@ void bwputs(char *s) {
 }
 
 void debug(unsigned int a, unsigned int b, unsigned int c) {
+	(void) a, (void) b, (void) c;
 	bwputs("debug halt\n");
 	while(1);
 }
@@ -55,22 +64,18 @@ int first_task(void) {
 	initialize a process given its stack
 	returns the pointer to the top of the stack (needed by activate)
 */
-unsigned int *init_process(unsigned int *stack, unsigned int size, int (*task)(void)) {
+unsigned int *init_process(struct Process *proc, unsigned int size, int (*task)(void)) {
 	/* this is necessary to run something in user mode */
-	stack[size-16] = task; /* (pc) program counter */
-	stack[size-15] = 0x10; /* (SPSR) saved state */
-	return stack + size - 16;
+	proc->stack[size-16] = (unsigned int)task; /* (pc) program counter */
+	proc->stack[size-15] = 0x10; /* (SPSR) saved state */
+	return proc->stack + size - 16;
 }
 
-unsigned int scheduler(unsigned int *procs[], unsigned int size, unsigned int active) {
-	unsigned int i;
-	if(procs[size-1] == procs[active]) return 0;
-	for(i = 0; i < size-1; i++) {
-		if(procs[i] == procs[active]) return i+1;
-	}
-	bwputs("scheduler error\n");
-	debug(procs, size, active);
-	return 0;
+/* basic */
+unsigned int scheduler(struct Process procs[], unsigned int num_procs, unsigned int active_proc) {
+	(void) procs;
+	if(num_procs-1 == active_proc) return 0;
+	return active_proc + 1;
 }
 
 void *memcpy(void* dest, void *src, size_t size) {
@@ -83,26 +88,26 @@ void *memcpy(void* dest, void *src, size_t size) {
 	return dest;
 }
 
-unsigned int _fork(unsigned int stacks[][STACK_SIZE], unsigned int *procs[], unsigned int parent, unsigned int next_pid) {
+unsigned int _fork(struct Process procs[], unsigned int parent_pid, unsigned int next_pid) {
 	unsigned int offset;
 	bwputs("_fork\n");
 	if(next_pid == NUM_STACKS) {
-		procs[parent][2+0] = -1;
+		procs[parent_pid].stackptr[2+0] = -1;
 		return next_pid;
 	}
-	offset = procs[parent] - stacks[parent];
-	procs[next_pid] = stacks[next_pid] + offset;
-	memcpy(procs[next_pid], procs[parent], sizeof(*procs[parent]) * (STACK_SIZE - offset));
-	procs[parent][2+0] = next_pid;
-	procs[next_pid][2+0] = 0;
+	offset = procs[parent_pid].stackptr - procs[parent_pid].stack;
+	procs[next_pid].stackptr = procs[next_pid].stack + offset;
+	memcpy(procs[next_pid].stackptr, procs[parent_pid].stackptr, sizeof(*(procs[parent_pid].stack)) * (STACK_SIZE - offset));
+	procs[parent_pid].stackptr[2+0] = next_pid;
+	procs[next_pid].stackptr[2+0] = 0;
 	return next_pid + 1;
 }
 
 int main(void) {
-	unsigned int user_stack[NUM_STACKS][STACK_SIZE];
-	unsigned int *procs[NUM_STACKS];
-	unsigned int n = 0; /* the next available stack. n should never be >= NUM_STACKS */
-	unsigned int active = 0;
+	struct Process procs[NUM_STACKS];
+	unsigned int num_procs = 0; /* the next available stack. n should never be >= NUM_STACKS */
+	unsigned int active_proc = 0;
+	unsigned int ic; /* interrupt condition */
 
 	/* initialize the hardware timer */
 	*(PIC + VIC_INTENABLE) = PIC_TIMER01;
@@ -110,19 +115,19 @@ int main(void) {
 	*(TIMER0 + TIMER_CONTROL) = TIMER_EN | TIMER_32BIT | TIMER_PERIODIC | TIMER_INTEN;
 
 	/* start running tasks */
-	procs[n] = init_process(user_stack[n],STACK_SIZE,&first_task);
-	n++;
+	procs[num_procs].stackptr = init_process(&(procs[num_procs]),STACK_SIZE,&first_task);
+	num_procs++;
 	while(1) {
 		/* choose a process to run */
-		active = scheduler(procs, n, active);
+		active_proc = scheduler(procs, num_procs, active_proc);
 
 		/* run the process for some time */
 		bwputs("activate\n");
-		procs[active] = activate(procs[active]);
+		procs[active_proc].stackptr = activate(procs[active_proc].stackptr);
 
 		/* handle the interrupt */
-		unsigned int val = procs[active][2+7]; /* the interrupt value */
-		if(val == PIC) {
+		ic = procs[active_proc].stackptr[2+7]; /* the interrupt value */
+		if(ic == (unsigned int)PIC) {
 			bwputs("hardware interrupt\n");
 			if(*PIC & PIC_TIMER01) {
 				if(*(TIMER0 + TIMER_MIS)) {
@@ -130,14 +135,14 @@ int main(void) {
 					*(TIMER0 + TIMER_INTCLR) = 1;
 				}
 			}
-		} else if(val == &yield) {
+		} else if(ic == (unsigned int)&yield) {
 			bwputs("yield\n");
-		} else if(val == &fork) {
-			n = _fork(user_stack, procs, active, n);
+		} else if(ic == (unsigned int)&fork) {
+			num_procs = _fork(procs, active_proc, num_procs);
 			bwputs("fork\n");
-		} else if(val == &write) {
+		} else if(ic == (unsigned int)&write) {
 			bwputs("write\n");
-		} else if(val == &read) {
+		} else if(ic == (unsigned int)&read) {
 			bwputs("read\n");
 		} else {
 			bwputs("UNKNOWN SYSCALL\n");
